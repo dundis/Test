aba7ad56d86d8bf18e0d23aaa94cb2408e39128b
dundis <dundi_s@yahoo.com>
2018-10-24 12:39:16 +0300

    Initial commit

---
+++ src/a/b/c/Test.java
@@ -0,0 +1,13 @@
+package a.b.c;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+	}
+
+}
